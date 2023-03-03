a489a811c28716c0c0b2bc81018f17efd5042a1d
mcdestefano <124924561+mcdestefano@users.noreply.github.com>
2023-03-03 13:08:09 -0500

    Implement playRound, create and implement resultHelper

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 /* function getComputerChoice
    randomly returns rock paper or scissors
 
+   function resultHelper(pS, cS, res)
+   returns string with player seletion, computer selection, and result
+     in approporiate places
+
    function playRound(playerSelection, computerSelection)
    plays one round of rock paper scissors taking in playerSelection
      and computer selection as parameters
@@ -29,19 +33,92 @@ if (num == 0){
 }
 }
 
+// resultHelper(pS, cS, res)
+function resultHelper(pS, cS, res = "Tie"){
+// if res is win returns "You Won! ${pS} beats ${cS}."
+    if (res == "Win"){
+        return `You Won! ${pS} beats ${cS}.`;
+// if res is loss returns "You Lost! ${cS} beats ${pS}."
+    } else if (res == "Loss"){
+        return `You Lost! ${cS} beats ${pS}.`;
+// else return "You Tied!"
+    } else {
+        return "You Tied!";
+    }
+}
+
 // playRound(playerSelection, computerSelection)
+function playRound(playerSelection, computerSelection){
+// convert player selection to appropriate case
+let first = playerSelection.substring(0, 1);
+first = first.toUpperCase();
+let rest = playerSelection.substring(1);
+rest = rest.toLowerCase();
+playerSelection = first + rest;
+// create variable for result
+let result;
+switch (playerSelection){
 // if pS is rock
-//   if cS is rock return tie
-//   if cS is paper return loss
-//   if cS is scissors return win
+    case "Rock":
+        switch (computerSelection){
+//   if cS is rock result is tie
+            case "Rock":
+                result = "Tie";
+                break;
+//   if cS is paper result is loss
+            case "Paper":
+                result = "Loss";
+                break;
+//   if cS is scissors result is win
+            case "Scissors":
+                result = "Win";
+                break;
+        }
+        break;
 // if pS is paper
-//   if cS is paper return tie
-//   if cS is scissors return loss
-//   if cS is rock return win
+    case "Paper":
+        switch (computerSelection){
+//   if cS is rock result is win
+            case "Rock":
+                result = "Win";
+                break;
+//   if cS is paper result is tie
+            case "Paper":
+                result = "Tie";
+                break;
+//   if cS is scissors result is loss
+            case "Scissors":
+                result = "Loss";
+                break;
+        }
+        break;
 // if pS is scissors
-//   if cS is scissors return tie
-//   if cS is rock return loss
-//   if cS is paper return win
+    case "Scissors":
+        switch (computerSelection){
+//   if cS is rock result is loss
+            case "Rock":
+                result = "Loss";
+                break;
+//   if cS is paper result is win
+            case "Paper":
+                result = "Win";
+                break;
+//   if cS is scissors result is tie
+            case "Scissors":
+                result = "Tie";
+                break;
+        }
+        break;
+}
+// returns result string
+return resultHelper(playerSelection, computerSelection, result);
+}
+
+/* below is for testing playRound
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
+*/
 
 // game()
 // create variables for wins, losses, and ties
