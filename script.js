833cb68a20556892ab2d10fc313d371cb97b6846
mcdestefano <124924561+mcdestefano@users.noreply.github.com>
2023-03-03 12:28:50 -0500

    Explain problem in js comments

---
+++ script.js
@@ -0,0 +1,12 @@
+// function getComputerChoice
+// randomly returns rock paper or scissors
+
+// function playRound(playerSelection, computerSelection)
+// plays one round of rock paper scissors taking in playerSelection
+//   and computer selection as parameters
+// returns string with result e.g. "You Lose! Paper beats Rock"
+// player selection should be case insensitive
+
+// function game()
+// calls playRound 5 times and keeps score, then reports winner
+//   or loser at the end (prints to console)
